ddf7f052bdde2ae7b44adb7ca30ce5c007db04c6
Shibu Kalluvila Raj <shibu.raj@gmail.com>
2014-12-31 12:06:59 -0600

    Added the weather api

--- main.go
+++ main.go
@@ -2,9 +2,53 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/weather/", handleWeather)
+	http.ListenAndServe(":8080", nil)
+
+	//fmt.Println("Hello World!")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world !"))
+}
+
+func handleWeather(w http.ResponseWriter, r *http.Request) {
+	city := strings.SplitN(r.URL.Path, "/", 3)[2]
+	data, err := query(city)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	json.NewEncoder(w).Encode(data)
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func query(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+	defer resp.Body.Close()
+
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
 }
